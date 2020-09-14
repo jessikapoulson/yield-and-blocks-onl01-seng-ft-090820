8da23d9d7dac39faa1ea0b4cccc5268140484af4
Jessi Poulson <jessipoulson@hotmail.com>
2020-09-14 22:38:55 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,18 @@
 ["Tim", "Tom", "Jim"]
 
 def hello_t (array)
-  i= 0 #counter variable 
+  if block_given?
+    i= 0 #counter variable 
   
   while i < array.length 
     yield(array[i])
     i=i+1 #increment value of 1, i equals a given index number of our array
   end
   
-  array
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
