3c8c1ce83e3a6b977d8ce75209847041fd26ffd9
Jessi Poulson <jessipoulson@hotmail.com>
2020-09-14 22:32:21 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,10 @@
-def hello_t
-
+def hello_t (array)
+  i= 0 #counter variable 
+  
+  while i < array.length 
+    yield array [i] #bracket method to grab the value of each successive index element of loop.
+    i=i+1 #increment value of 1, i equals a given index number of our array
+  end
 end
 
 # call your method here!
